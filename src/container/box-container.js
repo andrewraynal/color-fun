94587b3e1a827bf8d780f129c067fbf8c8c6a4d0
andrewraynal <andrewraynal@gmail.com>
2018-04-05 15:28:14 -0400

    react color tutorial

---
+++ src/container/box-container.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import * as actionCreators from '../actions/index.js';
+import Box from '../components/box.js';
+class BoxCon extends React.Component {
+  render() {
+    return <Box handleClick={this.props.loadColor} color={this.props.color} />;
+  }
+}
+const mapStateToProps = state => {
+  return state;
+};
+export default connect(mapStateToProps, actionCreators)(BoxCon);
